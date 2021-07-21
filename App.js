d33ef8a1bb429654fc7d176d09339bc795c3576f
Walter Rodriguez (Sendbird) <walter.rodriguez@sendbird.com>
2021-07-21 13:55:13 +0100

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, Component } from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+/**
+ * Sendbird
+ */
+import SendBird from 'sendbird';
+var sb = null;
+export default class App extends Component {
+
+    componentDidMount() {
+        /**
+         * Sendbird. Add your application ID
+         */
+        sb = new SendBird({appId: 'ENTER HERE YOUR APPLICATION ID'});
+        /**
+         * Connect using any of your users
+         */
+        sb.connect('ANY USER ID HERE', (user, error) => {
+            if (error) {
+                console.dir(error);
+                error = error
+            } else {
+                alert('You are connected :-)');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Sendbird test.</Text>
+                <Text>You should see an alert if connected.</Text>
+                <StatusBar style="auto" />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
